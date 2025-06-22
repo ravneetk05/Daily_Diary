6a32b0b008da6e40add3038c99c118201b516e8f
ravneetk05 <157576316+ravneetk05@users.noreply.github.com>
2025-06-22 19:47:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 // MongoDB connection
-mongoose.connect('mongodb://127.0.0.1:27017', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log('✅ Connected to MongoDB'))
