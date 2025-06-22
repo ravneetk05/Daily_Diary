1062d63cce50f7b1bf965b396a1711e8aee8b3d5
ravneetk05 <157576316+ravneetk05@users.noreply.github.com>
2025-06-22 19:49:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -73,6 +73,8 @@ app.post('/delete/:id', async (req, res) => {
 });
 
 // to start the server 
-app.listen(4000, () => {
-  console.log(' Server running at http://localhost:4000');
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
